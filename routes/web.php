efd5ddb30c709f4f26b9442b53ad9cab48238cf7
brenonalmeida <brenonascialmeida@gmail.com>
2023-02-01 17:31:02 -0300

    perf: adicionando middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SubForumController;
 use App\Http\Controllers\AlunoSubforumController;
+use App\Http\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,30 +21,38 @@ use App\Http\Controllers\AlunoSubforumController;
 */
 
 Route::get('/', function () {
+
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
     return view('dashboard');
 });
 
 Route::get ('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/dashboard-aluno', [DashboardController::class, 'aluno'])->name('dashboard-aluno');
-Route::get('/dashboard-professor', [DashboardController::class, 'professor'])->name('dashboard-professor');
-Route::get('/dashboard-superuser', [DashboardController::class, 'superuser'])->name('dashboard-superuser');
+
+//Rotas de dashboard de acordo com o tipo de usuário logado
+Route::get('/dashboard-aluno', [DashboardController::class, 'aluno'])->name('dashboard-aluno')->middleware('auth');
+Route::get('/dashboard-professor', [DashboardController::class, 'professor'])->name('dashboard-professor')->middleware('auth');
+Route::get('/dashboard-superuser', [DashboardController::class, 'superuser'])->name('dashboard-superuser')->middleware('auth');
+
+
+//Rotas de subforum
 Route::get('/{subforum}/subforum', [subforumController::class, 'show'])->name('subforum');
-Route::get('/{subforum}/detalhes-subforum', [subforumController::class, 'detalhar'])->name('detalhes-subforum');
-
-Route::post('/cadastrar-subforum', [SubForumController::class, 'criar'])->name('cadastrar-subforum');
-
-Route::post('/criar-post', [PostController::class, 'criar_post'])->name('criar-post');
-Route::post('/cadastrar-resposta', [PostController::class, 'criarResposta'])->name('cadastrar-resposta');
+Route::get('/{subforum}/detalhes-subforum', [subforumController::class, 'detalhar'])->name('detalhes-subforum')->middleware('professor-autenticado');
+Route::post('/cadastrar-subforum', [SubForumController::class, 'criar'])->name('cadastrar-subforum')->middleware('professor-autenticado');
+Route::post('/apagar-subforum/{id}', [SubForumController::class, 'apagar'])->name('apagar-subforum')->middleware('superuser-autenticado');
 
 
-Route::get('/{subforum}/vincular', [AlunoSubforumController::class, 'show'])->name('vincular-alunos');
-Route::post('/vincular', [AlunoSubforumController::class, 'vincular'])->name('vincular');
-Route::post('/desvincular', [AlunoSubforumController::class, 'desvincular'])->name('desvincular');
+//Rotas de vinculação de alunos com subforum
+Route::get('/{subforum}/vincular', [AlunoSubforumController::class, 'show'])->name('vincular-alunos')->middleware('professor-autenticado');
+Route::post('/vincular', [AlunoSubforumController::class, 'vincular'])->name('vincular')->middleware('professor-autenticado');
+Route::post('/desvincular', [AlunoSubforumController::class, 'desvincular'])->name('desvincular')->middleware('professor-autenticado');
 
-//Route::get('/{subforum}/desvincular', [AlunoSubforumController::class, 'desvincular'])->name('desvincular');
-
-Route::put('/trancar/{id}', [PostController::class, 'trancar'])->name('trancar');
-Route::put('/destrancar/{id}', [PostController::class, 'destrancar'])->name('destrancar');
+//Rotas de post
+Route::put('/trancar/{id}', [PostController::class, 'trancar'])->name('trancar')->middleware('professor-autenticado');
+Route::put('/destrancar/{id}', [PostController::class, 'destrancar'])->name('destrancar')->middleware('professor-autenticado');
+Route::post('/criar-post', [PostController::class, 'criar_post'])->name('criar-post')->middleware('auth');
+Route::post('/cadastrar-resposta', [PostController::class, 'criarResposta'])->name('cadastrar-resposta')->middleware('auth');
 
 
 require __DIR__.'/auth.php';
